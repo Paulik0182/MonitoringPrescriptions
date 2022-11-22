9db7e7e8505e27c820845dc741c5f37b6400b666
Paul <paulik0182@gmail.com>
2022-11-22 22:16:09 +0300

    correction build.gradle

--- app/src/main/java/com/example/monitoringprescriptions/RootActivity.kt
+++ app/src/main/java/com/example/monitoringprescriptions/RootActivity.kt
@@ -3,7 +3,7 @@ package com.example.monitoringprescriptions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
